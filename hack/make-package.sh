0e2fe5445505921f2e73d5622177b78707a02fd0
Zexi Li <zexi.li@qq.com>
2019-09-21 18:17:38 +0800

    add make rpm simple tool

---
+++ hack/make-package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+declare -r REPO_ROOT
+OUTPUT_DIR="$REPO_ROOT/_output"
+PKG_DIR="$OUTPUT_DIR/pkg"
+YUNION_BIN="$PKG_DIR/opt/yunion/bin"
+TAG="$(git describe --tags --abbrev=0)"
+
+cd "${REPO_ROOT}"
+
+make
+
+mkdir -p $YUNION_BIN
+
+cp -a "$OUTPUT_DIR/bin/ocadm" "$YUNION_BIN"
+
+docker run --rm -v "$(pwd):/src/" cdrx/fpm-centos:7 fpm  -n yunion-ocadm -v "${TAG#v}" -s dir -t rpm  -C "_output/pkg"
